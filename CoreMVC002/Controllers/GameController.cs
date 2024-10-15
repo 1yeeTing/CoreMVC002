fcc063db22f43b3f7c6e1205a649dffc9d28673a
1yeeting <clementgg5210@gmail.com>
2024-10-15 22:38:48 +0800

    加入專案檔案。

---
+++ CoreMVC002/Controllers/GameController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using CoreMVC002.Models;
+using Newtonsoft.Json;
+
+namespace CoreMVC002.Controllers
+{
+    public class GameController : Controller
+    {
+        private const string GameStateKey = "GameState";
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var model = GetOrCreateGameState();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Guess(XAXBEngine model)
+        {
+            var gameModel = GetOrCreateGameState();
+            gameModel.Guess = model.Guess;
+
+            if (!IsValidGuess(model.Guess))
+            {
+                ModelState.AddModelError("Guess", "請輸入四個數字。");
+                return View("Index", gameModel);
+            }
+
+            int numA = gameModel.numOfA(gameModel.Guess);
+            int numB = gameModel.numOfB(gameModel.Guess);
+            gameModel.Result = $"{numA}A{numB}B";
+
+            gameModel.AddGuessToHistory(gameModel.Guess, gameModel.Result);
+
+            if (gameModel.IsGameOver(gameModel.Guess))
+            {
+                TempData["GameOver"] = "true";
+                TempData["PlayAgainMessage"] = "你猜對了！是否要重新開始？";
+                TempData.Remove(GameStateKey);
+            }
+            else
+            {
+                SaveGameState(gameModel);
+            }
+
+            return View("Index", gameModel);
+        }
+
+        private XAXBEngine GetOrCreateGameState()
+        {
+            if (TempData[GameStateKey] is string serializedState)
+            {
+                TempData.Keep(GameStateKey);
+                return JsonConvert.DeserializeObject<XAXBEngine>(serializedState);
+            }
+
+            var newGame = new XAXBEngine(GenerateSecretNumber());
+            SaveGameState(newGame);
+            return newGame;
+        }
+
+        private void SaveGameState(XAXBEngine gameState)
+        {
+            TempData[GameStateKey] = JsonConvert.SerializeObject(gameState);
+        }
+
+        private string GenerateSecretNumber()
+        {
+            Random random = new Random();
+            return new string(Enumerable.Range(0, 4)
+                .Select(x => random.Next(0, 10).ToString()[0])
+                .ToArray());
+        }
+
+        private bool IsValidGuess(string guess)
+        {
+            return !string.IsNullOrEmpty(guess) && guess.Length == 4 && guess.All(char.IsDigit);
+        }
+    }
+}
---
+++ CoreMVC002/Models/XAXBEngine.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace CoreMVC002.Models
+{
+    public class XAXBEngine
+    {
+        [Newtonsoft.Json.JsonProperty]
+        public string Secret { get; set; }
+
+        [Newtonsoft.Json.JsonProperty]
+        public string Guess { get; set; }
+
+        [Newtonsoft.Json.JsonProperty]
+        public string Result { get; set; }
+
+        [Newtonsoft.Json.JsonProperty]
+        public List<string> GuessHistory { get; set; }
+
+        [Newtonsoft.Json.JsonProperty]
+        public int GuessCount { get; set; }
+
+        [Newtonsoft.Json.JsonConstructor]
+        public XAXBEngine()
+        {
+            Secret = GenerateRandomSecret();
+            Guess = null;
+            Result = null;
+            GuessHistory = new List<string>();
+            GuessCount = 0;
+        }
+
+        public XAXBEngine(string secretNumber)
+        {
+            Secret = secretNumber;
+            Guess = null;
+            Result = null;
+            GuessHistory = new List<string>();
+            GuessCount = 0;
+        }
+
+        private string GenerateRandomSecret()
+        {
+            Random random = new Random();
+            return new string(Enumerable.Range(0, 4)
+                .Select(x => random.Next(0, 10).ToString()[0])
+                .ToArray());
+        }
+
+        public int numOfA(string guessNumber)
+        {
+            int countA = 0;
+            for (int i = 0; i < Secret.Length; i++)
+            {
+                if (Secret[i] == guessNumber[i])
+                    countA++;
+            }
+            return countA;
+        }
+
+        public int numOfB(string guessNumber)
+        {
+            int countB = 0;
+            for (int i = 0; i < guessNumber.Length; i++)
+            {
+                if (Secret.Contains(guessNumber[i]) && Secret[i] != guessNumber[i])
+                    countB++;
+            }
+            return countB;
+        }
+
+        public bool IsGameOver(string guessNumber)
+        {
+            return numOfA(guessNumber) == 4;
+        }
+
+        public void AddGuessToHistory(string guess, string result)
+        {
+            GuessHistory.Add($"{guess} -> {result}");
+            GuessCount++;
+        }
+    }
+}
